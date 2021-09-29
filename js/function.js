ea422bf33d5bf351d9e8c66c5cd405aaca89e21d
gabrizz8998 <gabriel.lucia.vel@gmail.com>
2021-09-29 10:35:15 +0200

    c1

--- js/function.js
+++ js/function.js
@@ -1,54 +1,39 @@
-var texto=document.getElementById("numero");
-
 function convertir(){
-    var numero=texto.value;
-    
+    var numero=document.getElementById("numero").value;
     var opcion = document.getElementById("conversion").value;
-    //alert(numero);
-    //alert(opcion);
     var resultado;
     switch(opcion){
-        case "Decimal_binario":{
-            //alert("Decimal_binario")
+        case "1":{
             var number=parseInt(numero);
-            alert(numero.toString(2))
-            //resultado=numero.toString(2);
-           // alert(resultado);
+            resultado=number.toString(2);
+           
             break;
         }
-        case "Decimal_hexadecimal":{
-            //alert("Decimal_hexadecimal")
-            resultado=numero.toString(16);
-            alert(resultado);
+        case "2":{
+            var number=parseInt(numero);
+            resultado=number.toString(16);
             break;
         }
-        case "Decimal_octal":{
-            //alert("Decimal_octal")
-            resultado=numero.toString(8);
-            alert(resultado);
+        case "3":{
+            var number=parseInt(numero);
+            resultado=number.toString(8);
             break;
         }
-        case "Binario_decimal":{
-            //alert("Binario_decimal")
+        case "4":{
             resultado=parseInt(numero,2);
-            alert(resultado);
             break;
         }
-        case "Hexadecimal_decimal":{
-            //alert("Hexadecimal_decimal")
+        case "5":{
             resultado=parseInt(numero,16);
-            alert(resultado);
             break;
         }
-        case "Octal_decimal":{
-            //alert("Octal_decimal")
+        case "6":{
             resultado=parseInt(numero,8);
-            alert(resultado);
             break;
         }
         default :{break;}
     }
-    alert(resultado);
+    document.getElementById("resultado").value=resultado;
 
 }
 
