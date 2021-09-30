03335100b17ec9b3363beec8f26c9d3286597db1
gabrizz8998 <gabriel.lucia.vel@gmail.com>
2021-09-30 08:58:46 +0200

    c

--- js/function.js
+++ js/function.js
@@ -2,19 +2,17 @@ function convertir(){
     var numero=document.getElementById("numero").value;
     var opcion = document.getElementById("conversion").value;
     var resultado;
+    var number=parseInt(numero);
     switch(opcion){
         case "1":{
-            var number=parseInt(numero);
             resultado=number.toString(2);
             break;
         }
         case "2":{
-            var number=parseInt(numero);
             resultado=number.toString(16);
             break;
         }
         case "3":{
-            var number=parseInt(numero);
             resultado=number.toString(8);
             break;
         }
